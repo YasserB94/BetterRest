e4e44724f1401159d70f7b4a413de9aac017804e
Yasser Bal <yasserbal@icloud.com>
2023-11-12 19:55:55 +0100

    Basic view setup

--- BetterRest/ViewModels/MainViewModel.swift
+++ BetterRest/ViewModels/MainViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class MainViewModel:ObservableObject
+{
+    public let MAX_HOURS_SLEEP = 12.0
+    public let MIN_HOURS_SLEEP = 4.0
+    
+    public let MAX_CUPS_COFFEE = 20
+    public let MIN_CUPS_COFFEE = 0
+    
+    @Published var wakeDate:Date = Date.now;
+    @Published var amountOfSleepInHours:Double = 8.0
+    @Published var cupsOfCoffeeDrank = 1
+}
--- BetterRest/Views/MainView.swift
+++ BetterRest/Views/MainView.swift
@@ -8,22 +8,46 @@
 import SwiftUI
 
 struct MainView: View {
-    @State private var sleepAmount = 0.0
-    @State private var wakeUp = Date.now
+    @ObservedObject var vm = MainViewModel()
 
     
     var body: some View {
         NavigationStack{
             VStack{
-                DatePicker("Please enter a date", selection: $wakeUp)
-                    .pickerStyle(.wheel)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-
+                Text(
+                    "When would you like to wake up?"
+                )
+                    .font(.headline)
+                DatePicker(
+                    "Please enter a time",
+                    selection: $vm.wakeDate,
+                    displayedComponents: .hourAndMinute
+                )
+                    .labelsHidden()
+                Text("Howmany hours would you like to sleep?")
+                Stepper(
+                    "\(vm.amountOfSleepInHours.formatted())",
+                    value: $vm.amountOfSleepInHours,
+                    in:vm.MIN_HOURS_SLEEP...vm.MAX_HOURS_SLEEP,
+                    step:0.25
+                )
+                Text("Cups of coffee drank per day")
+                
+                Stepper(
+                    "\(vm.cupsOfCoffeeDrank) cup(s)",
+                    value:$vm.cupsOfCoffeeDrank,
+                    in:vm.MIN_CUPS_COFFEE...vm.MAX_CUPS_COFFEE
+                )
             }
             .padding(.horizontal,30)
             .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Resolve",action: calculateBedtime)
+            }
         }
-        
+    }
+    func calculateBedtime(){
+        print("World!")
     }
 }
 
